50bb6b0fe2207781bdb7c652c87b57c9a82f8f8f
Haisto <1968621421@qq.com>
2026-04-11 14:52:10 +0800

    feat(action): 添加版本参数支持并重构下载逻辑
    - 添加 version 输入参数用于指定 musl-cross-make 版本
    - 更新下载 URL 格式以支持版本化 releases
    - 修改缓存查找和存储逻辑以包含版本信息
    - 重构代码结构提高可读性
    - 更新 action 配置文件添加新参数定义

--- index.js
+++ index.js
@@ -4,101 +4,93 @@ const exec = require("@actions/exec");
 const io = require("@actions/io");
 const path = require("path");
 
-const target = core.getInput("target", { required: true });
-const variant = core.getInput("variant", { required: true });
+const toolchainName = "haisto-musl-cross-make";
+const target = core.getInput("target", {required: true});
+const version = core.getInput("version", {required: true});
+const variant = core.getInput("variant", {required: true});
 const escapedVariant = variant.replace("/", "_");
 const build = core.getInput("build").toUpperCase() === "TRUE";
 const buildDir = path.join("/opt/", target, variant);
 
-const tags = {
-  "pmmp/musl-cross-make": "heracles",
-  "richfelker/musl-cross-make": "heracles",
-};
-
 (async () => {
-  try {
-    const url = `https://github.com/nao20010128nao/musl-cross-compilers/releases/download/${tags[variant]}/output-${target}-${variant.replace(
-      "/",
-      "_"
-    )}.tar.gz`;
-    console.log(`Downloading ${url}`);
+    try {
+        const versionSpec = `${version}-${escapedVariant}`
+        console.log(`Installing ${toolchainName} ${versionSpec}`);
+        // output-aarch64_be-linux-musl-pmmp_musl-cross-make.tar.gz
+        const downloadUrl = `https://github.com/haisto/musl-cross-compilers/releases/download/${version}/output-${target}-${escapedVariant}.tar.gz`
+        console.log(`Downloading ${downloadUrl}`);
 
-    let cachedPath;
-    if (build) {
-      const destDir = buildDir;
-      await io.mkdirP(destDir);
-      // https://stackoverflow.com/questions/11912878/gcc-error-gcc-error-trying-to-exec-cc1-execvp-no-such-file-or-directory
-      let ret = await exec.exec("sudo", ["apt", "update"], {
-        ignoreReturnCode: true,
-      });
-      if (ret != 0) {
-        console.error(`apt update failed with code ${ret}`);
-      }
+        let cachedPath;
+        if (build) {
+            const destDir = buildDir;
+            await io.mkdirP(destDir);
+            // https://stackoverflow.com/questions/11912878/gcc-error-gcc-error-trying-to-exec-cc1-execvp-no-such-file-or-directory
+            let ret = await exec.exec("sudo", ["apt", "update"], {
+                ignoreReturnCode: true,
+            });
+            if (ret != 0) {
+                console.error(`apt update failed with code ${ret}`);
+            }
 
-      ret = await exec.exec("sudo", ["apt", "install", "--reinstall", "gcc", "g++", "cpp-11", "cpp-9"], {
-        ignoreReturnCode: true,
-      });
-      if (ret != 0) {
-        console.error(`apt install failed with code ${ret}`);
-      }
+            ret = await exec.exec("sudo", ["apt", "install", "--reinstall", "gcc", "g++", "cpp-11", "cpp-9"], {
+                ignoreReturnCode: true,
+            });
+            if (ret != 0) {
+                console.error(`apt install failed with code ${ret}`);
+            }
 
-      ret = await exec.exec("git", ["clone", `https://github.com/${variant}.git`, destDir], {
-        ignoreReturnCode: true,
-      });
-      if (ret != 0) {
-        throw new Error(`git clone failed with code ${ret}`);
-      }
+            ret = await exec.exec("git", ["clone", `https://github.com/${variant}.git`, destDir], {
+                ignoreReturnCode: true,
+            });
+            if (ret != 0) {
+                throw new Error(`git clone failed with code ${ret}`);
+            }
 
-      ret = await exec.exec("make", ["-j4"], {
-        cwd: destDir,
-        ignoreReturnCode: true,
-        env: {
-          TARGET: target,
-        },
-      });
-      if (ret != 0) {
-        throw new Error(`make -j4 failed with code ${ret}`);
-      }
+            ret = await exec.exec("make", ["-j4"], {
+                cwd: destDir, ignoreReturnCode: true, env: {
+                    TARGET: target,
+                },
+            });
+            if (ret != 0) {
+                throw new Error(`make -j4 failed with code ${ret}`);
+            }
 
-      ret = await exec.exec("make", ["install"], {
-        cwd: destDir,
-        ignoreReturnCode: true,
-        env: {
-          TARGET: target,
-        },
-      });
-      if (ret != 0) {
-        throw new Error(`make install failed with code ${ret}`);
-      }
-      cachedPath = destDir;
-    } else {
-      cachedPath = tc.find("mcm", `${target}-${escapedVariant}`);
-      console.log('find cache', `${target}-${escapedVariant}`, cachedPath)
+            ret = await exec.exec("make", ["install"], {
+                cwd: destDir, ignoreReturnCode: true, env: {
+                    TARGET: target,
+                },
+            });
+            if (ret != 0) {
+                throw new Error(`make install failed with code ${ret}`);
+            }
+            cachedPath = destDir;
+        } else {
+            cachedPath = tc.find(toolchainName, versionSpec);
+        }
+        if (cachedPath) {
+            console.log(`Found installation at ${cachedPath}`);
+        } else {
+            const toolchainPath = await tc.downloadTool(url);
+            const toolchainExtractedFolder = await tc.extractTar(toolchainPath);
+            cachedPath = await tc.cacheDir(toolchainExtractedFolder, toolchainPath, versionSpec);
+            console.log(`Installed at ${cachedPath}`);
+        }
+        cachedPath = path.join(cachedPath, "output", "bin");
+        console.log(`Binaries are at ${cachedPath}`);
+        core.addPath(cachedPath);
+        core.setOutput("path", cachedPath);
+    } catch (e) {
+        if (build) {
+            console.log("Build error occured and uploading build directory as artifacts");
+            await exec.exec("tar", ["-czf", "/opt/mcm.tar.gz", buildDir]);
+            const artifact = require("@actions/artifact");
+            const artifactClient = artifact.create();
+            const artifactName = `musl-cross-compiler-error-${target}-${variant.replace("/", "_")}`;
+            const files = ["/opt/mcm.tar.gz"];
+            const rootDirectory = "/opt/";
+            const options = {};
+            await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
+        }
+        core.setFailed(e);
     }
-    if (cachedPath) {
-      console.log(`Found installation at ${cachedPath}`);
-    } else {
-      const toolchainPath = await tc.downloadTool(url);
-      const toolchainExtractedFolder = await tc.extractTar(toolchainPath);
-      cachedPath = await tc.cacheDir(toolchainExtractedFolder, "mcm", `${target}-${escapedVariant}`);
-      console.log(`Installed at ${cachedPath}`);
-    }
-    cachedPath = path.join(cachedPath, "output", "bin");
-    console.log(`Binaries are at ${cachedPath}`);
-    core.addPath(cachedPath);
-    core.setOutput("path", cachedPath);
-  } catch (e) {
-    if (build) {
-      console.log("Build error occured and uploading build directory as artifacts");
-      await exec.exec("tar", ["-czf", "/opt/mcm.tar.gz", buildDir]);
-      const artifact = require("@actions/artifact");
-      const artifactClient = artifact.create();
-      const artifactName = `musl-cross-compiler-error-${target}-${variant.replace("/", "_")}`;
-      const files = ["/opt/mcm.tar.gz"];
-      const rootDirectory = "/opt/";
-      const options = {};
-      await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
-    }
-    core.setFailed(e);
-  }
 })();
